daa589b750035aa5b36c051876da9cca125e8ff0
Erika <erika@test.com>
2026-09-01 19:42:40 +0000

    feat: Add pie chart skeleton

---
+++ frontend/src/app/features/dashboard/components/pie-chart/pie-chart.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pie-chart',
+  styleUrl: './pie-chart.css',
+  templateUrl: './pie-chart.html',
+})
+export class PieChart {}
