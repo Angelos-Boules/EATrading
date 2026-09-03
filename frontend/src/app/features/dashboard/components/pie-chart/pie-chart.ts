9bca04ede2a4fbaa84f3efba95e1b214546bc7c8
Erika <erika@test.com>
2026-09-03 15:21:48 +0000

    created pie chart

--- frontend/src/app/features/dashboard/components/pie-chart/pie-chart.ts
+++ frontend/src/app/features/dashboard/components/pie-chart/pie-chart.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AgCharts } from 'ag-charts-angular';
+import { AllCommunityModule, ModuleRegistry, AgChartOptions, PieSeriesModule } from 'ag-charts-community';
 
+ModuleRegistry.registerModules([AllCommunityModule, PieSeriesModule]);
 @Component({
-  imports: [],
+  standalone: true,
+  imports: [AgCharts],
   selector: 'app-pie-chart',
-  styleUrl: './pie-chart.css',
+  styleUrls: ['./pie-chart.css'],
   templateUrl: './pie-chart.html',
 })
-export class PieChart {}
+export class PieChartComponent {
+  public chartOptions: AgChartOptions = {
+    title: {
+      text: 'Pie Chart Example',
+      fontSize: 18,
+    },
+    series: [
+      ({
+        type: 'pie',
+        labelKey: 'label',
+        angleKey: 'value',
+      } as any)
+    ]
+  };
+  @Input()
+  public set data(value: any[] | undefined) {
+    if (!value) return;
+    // replace chartOptions object so AgCharts picks up the change
+    this.chartOptions = {
+      ...this.chartOptions,
+      data: value,
+    } as AgChartOptions;
+  }
+}
